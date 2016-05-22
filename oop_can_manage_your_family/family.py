f4a52b32b5eb56aeb04f922bab8138b7015d970a
Tasneem Farag <tasneem.a.farag@gmail.com>
2016-05-21 22:51:35 -0700

    remove the comment

--- oop_can_manage_your_family/family.py
+++ oop_can_manage_your_family/family.py
@@ -499,7 +499,7 @@ if marc.is_married():
 
 save_to_file(my_family, "my_family.json")
 '''
-
+'''
 my_family = load_from_file("my_family.json")
 
 marc = my_family[0]
@@ -513,4 +513,5 @@ monica = vanessa.has_child_with(marc, 5, "Monica", [5, 6, 2016], "Female", "Blue
 print "Vanessa has %d children" % (len(vanessa.children))
 
 save_to_file(my_family, "my_family.json")
+'''
 
