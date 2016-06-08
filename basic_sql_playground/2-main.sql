5f6407a38aea15126db97a805925b16f063a5b36
tasneemfarag <39@holbertonschool.com>
2016-06-08 02:49:26 +0000

    done with task 4

---
+++ basic_sql_playground/2-main.sql
@@ -0,0 +1,5 @@
+INSERT INTO person (first_name,last_name,age)
+VALUES ('Jon', 'Snow', 26);
+INSERT INTO person (first_name,last_name,age)
+VALUES ('Arya', 'Stark', 12);
+SELECT * FROM Person ORDER BY last_name;
