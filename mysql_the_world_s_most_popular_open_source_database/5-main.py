7c8ad55c3b9db757ab891d00210856237eaa4e4c
Tasneem Farag <tasneem.a.farag@gmail.com>
2016-06-24 00:53:07 -0400

    done with task 5

--- mysql_the_world_s_most_popular_open_source_database/5-main.py
+++ mysql_the_world_s_most_popular_open_source_database/5-main.py
@@ -3,12 +3,21 @@ import mysql.connector
 cnx = mysql.connector.connect(user='student', password='aLQQLXGQp2rJ4Wy5',host='173.246.108.142',database='Project_169')
 cursor = cnx.cursor()
 
-query = ("SHOW tables")
+query = ("SELECT TVShow.name, Season.number, Episode.number, Episode.name FROM TVShow, Season, Episode WHERE Season.id = Episode.season_id AND TVShow.id = Season.tvshow_id ORDER BY TVShow.name, Season.number, Episode.number")
 
 cursor.execute(query)
-
-for table in cursor:
-  print "" + table
+result = cursor.fetchall() 
+TVShowName = []
+SeasonNumber = []
+for row in result:
+	if row[0] not in TVShowName:		
+  		print row[0] + ":"
+  		TVShowName.append(row[0])
+  		SeasonNumber = []
+  	if row[1] not in SeasonNumber:		
+  		print "\tSeason " + str(row[1]) + ":"
+  		SeasonNumber.append(row[1])	
+  	print "\t\t" + str(row[2]) + ": " + row[3]
 
 cursor.close()
 cnx.close()
